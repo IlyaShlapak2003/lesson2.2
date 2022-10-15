45ede35cbe0e006f261dcef0e4e570a99196efc9
IlyaShlapak2003 <gosha.shlapak6982@gmail.com>
2022-10-15 13:39:26 +0300

    First Commit

--- src/Auto.java
+++ src/Auto.java
@@ -3,17 +3,44 @@ public class Auto {
     String model;
     float engineVolume;
     String color;
-    int productionYear;
+    Integer productionYear;
     String productionCountry;
-    Auto(String brand, String model, float engineVolume,String color, int productionYear, String productionCountry){
-        this.brand=brand;
-        this.model=model;
-        this.engineVolume=engineVolume;
-        this.color=color;
-        this.productionYear=productionYear;
-        this.productionCountry=productionCountry;
+
+    Auto(String brand, String model, float engineVolume, String color, Integer productionYear, String productionCountry) {
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "defult";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5f;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == null) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
     }
-    void automobile(){
+
+    void automobile() {
         System.out.println(brand + " " + model + ", " + productionYear + " год выпуска, сборка в " + productionCountry + ", " + color + " цвет кузова, объем двигателя - " + engineVolume);
     }
 
