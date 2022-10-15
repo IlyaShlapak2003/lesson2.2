23bfacbab3c25b20447c180537a8a678129f0d0e
IlyaShlapak2003 <gosha.shlapak6982@gmail.com>
2022-10-15 14:50:25 +0300

    First Commit

--- src/Human.java
+++ src/Human.java
@@ -1,13 +1,35 @@
 public class Human {
-    int yearOfBiirth;
+    Integer yearOfBirth;
     String name;
     String town;
-    Human(int yearOfBiirth, String name, String town){
-        this.yearOfBiirth=yearOfBiirth;
-        this.name=name;
-        this.town=town;
+    String jobTitle;
+
+    Human(Integer yearOfBirth, String name, String town, String jobTitle) {
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        } else if (yearOfBirth == null) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
+        if (name == null) {
+            this.name = "'инормация не указана'";
+        } else {
+            this.name = name;
+        }
+        if (town == null) {
+            this.town = "'инормация не указана'";
+        } else {
+            this.town = town;
+        }
+        if (jobTitle == null) {
+            this.jobTitle = "'инормация не указана'";
+        } else {
+            this.jobTitle = jobTitle;
+        }
     }
-    void information(){
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + (2022 - yearOfBiirth) + " году. Будем знакомы!");
+
+    void information() {
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + (2022 - yearOfBirth) + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Human hum1 = new Human(35, "Максим", "Минск");
-        Human hum2 = new Human(29, "Аня", "Москва");
-        Human hum3 = new Human(28, "Катя", "Калининград");
-        Human hum4 = new Human(27, "Артем", "Москва");
+        Human hum1 = new Human(35, "Максим", "Минск","бренд-менеджера");
+        Human hum2 = new Human(29, "Аня", "Москва","мтодиста образовательных программ");
+        Human hum3 = new Human(28, "Катя", "Калининград","продакт-менеджера");
+        Human hum4 = new Human(27, null, "Москва","директора по развитию бизнеса");
         hum1.information();
         hum2.information();
         hum3.information();
