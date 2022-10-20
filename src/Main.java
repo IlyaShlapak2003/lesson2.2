019b5aa2ab07644fb95ff756aeecaac2b4fc756a
IlyaShlapak2003 <gosha.shlapak6982@gmail.com>
2022-10-20 17:52:04 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,84 @@
 import transport.Auto;
 
+import java.security.Key;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        Auto car1 = new Auto("Lada","Granta",1.7f, "жёлтый", 2015, "России", "автоматическая", "седан", "н987хо41",5,"летняя");
-        Auto car2 = new Auto("Audi","A8 50 L TDI quattro",3.0f,"черный",2020,"Германии", "автоматическая", "универсал", "н887хо46",5,"летняя");
-        Auto car3 = new Auto("BMW", "Z8",3.0f,"черный",2021,"Германии", "автоматическая", "минивен", "н937хо61",7,"зимняя");
-        Auto car4 = new Auto("Kia",null, 2.4f,"красный",2018,"Южной Корее", "механическая", "универсал", "н063хо40",4,"летняя");
-        Auto car5 = new Auto("Hyundai","Avante",0,"оранжевый",2016,"Южной Корее", "автоматическая", "внедорожник", "н987хо41",7,"зимняя");
-        car1.automobile();
-        car2.automobile();
-        car3.automobile();
-        car4.automobile();
-        car5.automobile();
+
+        Auto car1 = new Auto("Lada",
+                "Granta",
+                1.7f,
+                "жёлтый",
+                2015,
+                "России",
+                "автоматическая",
+                "седан",
+                "н987хо41",
+                5,
+                "летняя");
+        car1.setKey(new Auto.key(true,false));
+        car1.setInsurance(new Auto.insurance(LocalDate.now(),1254,"125463875"));
+        Auto.information(car1);
+
+        Auto car2 = new Auto("Audi",
+                "A8 50 L TDI quattro",
+                3.0f,
+                "черный", 2020,
+                "Германии",
+                "автоматическая",
+                "универсал",
+                "н887хо46",
+                5,
+                "летняя");
+        car2.setKey(new Auto.key(false,false));
+        car2.setInsurance(new Auto.insurance(LocalDate.now(),1254,"125463875"));
+        Auto.information(car2);
+
+        Auto car3 = new Auto("BMW",
+                "Z8",
+                3.0f,
+                "черный",
+                2021,
+                "Германии",
+                "автоматическая",
+                "минивен",
+                "н937хо61",
+                7,
+                "зимняя");
+        car3.setKey(new Auto.key(true,true));
+        car3.setInsurance(new Auto.insurance(LocalDate.now(),1254,"125463875"));
+        Auto.information(car3);
+
+        Auto car4 = new Auto("Kia",
+                null,
+                2.4f,
+                "красный",
+                2018,
+                "Южной Корее",
+                "механическая",
+                "универсал",
+                "н063хо40",
+                4,
+                "летняя");
+        car4.setKey(new Auto.key(false,true));
+        car4.setInsurance(new Auto.insurance(LocalDate.now(),1254,"125463875"));
+        Auto.information(car4);
+
+        Auto car5 = new Auto("Hyundai",
+                "Avante",
+                0,
+                "оранжевый",
+                2016,
+                "Южной Корее",
+                "автоматическая",
+                "внедорожник",
+                "н987хо41",
+                7,
+                "зимняя");
+        car5.setKey(new Auto.key(true,false));
+        car5.setInsurance(new Auto.insurance(LocalDate.now(),1254,"125463875"));
+        Auto.information(car5);
+
     }
 }
--- src/transport/Auto.java
+++ src/transport/Auto.java
@@ -1,6 +1,90 @@
 package transport;
 
+import java.time.LocalDate;
+
 public class Auto {
+
+    public static class insurance {
+        private final LocalDate validity;
+        private final double price;
+        private final String number;
+
+        public insurance() {
+            this(null, 10000, "123456789");
+        }
+
+        public insurance(LocalDate validity, Integer price, String number) {
+            if (validity == null) {
+                this.validity = LocalDate.now().plusDays(365);
+            } else {
+                this.validity = validity;
+            }
+            if (number == null) {
+                this.number = "123456789";
+            } else {
+                this.number = number;
+            }
+            this.price = price;
+        }
+
+        public LocalDate getValidity() {
+            return validity;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+
+        public void checkDay() {
+            if (validity.isBefore(LocalDate.now()) || validity.isEqual(LocalDate.now())) {
+                System.out.println("Нужно срочно ехать оформлять новую страховку");
+            }
+        }
+
+        public void checkNumber() {
+            if (number.length() != 9) {
+                System.out.println("Номер страховки некорректный!");
+            }
+        }
+
+    }
+
+    public static class key {
+        private boolean remoteEngineStart;
+        private boolean keylessEntry;
+
+        public key() {
+            this(false, false);
+        }
+
+        public key(boolean remoteEngineStart, boolean keylessEntry) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.keylessEntry = keylessEntry;
+
+
+        }
+
+        public boolean getRemoteEngineStart() {
+            return remoteEngineStart;
+        }
+
+        public void setRemoteEngineStart(boolean remoteEngineStart) {
+            this.remoteEngineStart = remoteEngineStart;
+        }
+
+        public boolean getKeylessEntry() {
+            return keylessEntry;
+        }
+
+        public void setKeylessEntry(boolean keylessEntry) {
+            this.keylessEntry = keylessEntry;
+        }
+    }
+
     private String brand;
     private String model;
     private float engineVolume;
@@ -12,9 +96,212 @@ public class Auto {
     private String registrationNumber;
     private Integer numberOfSeats;
     private String rubber;
+    private key key;
+    private insurance insurance;
 
+    public Auto(String brand, String model, float engineVolume, String color, Integer productionYear, String productionCountry, String transmission, String bodyType, String registrationNumber, Integer numberOfSeats, String rubber, key key, insurance insurance) {
+        if (insurance == null) {
+            this.insurance = new insurance();
+        } else {
+            this.insurance = insurance;
+        }
+        if (key == null) {
+            this.key = new key();
+        } else {
+            this.key = key;
+        }
+
+        if (transmission == null) {
+            this.transmission = "default";
+        } else {
+            this.transmission = transmission;
+        }
+        if (bodyType == null) {
+            this.bodyType = "default";
+        } else {
+            this.bodyType = bodyType;
+        }
+        if (registrationNumber == null) {
+            this.registrationNumber = "х000хх000";
+        } else {
+            this.registrationNumber = registrationNumber;
+        }
+        if (numberOfSeats <= 2) {
+            this.numberOfSeats = 5;
+        } else {
+            this.numberOfSeats = numberOfSeats;
+        }
+        if (rubber == null) {
+            this.rubber = "default";
+        } else {
+            this.rubber = rubber;
+        }
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "defult";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5f;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == null) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+    }
+
+    public Auto(String brand, String model, float engineVolume, String color, Integer productionYear, String productionCountry, String transmission, String bodyType, String registrationNumber, Integer numberOfSeats, String rubber, insurance insurance) {
+        if (insurance == null) {
+            this.insurance = new insurance();
+        } else {
+            this.insurance = insurance;
+        }
+
+        if (transmission == null) {
+            this.transmission = "default";
+        } else {
+            this.transmission = transmission;
+        }
+        if (bodyType == null) {
+            this.bodyType = "default";
+        } else {
+            this.bodyType = bodyType;
+        }
+        if (registrationNumber == null) {
+            this.registrationNumber = "х000хх000";
+        } else {
+            this.registrationNumber = registrationNumber;
+        }
+        if (numberOfSeats <= 2) {
+            this.numberOfSeats = 5;
+        } else {
+            this.numberOfSeats = numberOfSeats;
+        }
+        if (rubber == null) {
+            this.rubber = "default";
+        } else {
+            this.rubber = rubber;
+        }
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "defult";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5f;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == null) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+    }
+
+    public Auto(String brand, String model, float engineVolume, String color, Integer productionYear, String productionCountry, String transmission, String bodyType, String registrationNumber, Integer numberOfSeats, String rubber, key key) {
+        if (key == null) {
+            this.key = new key();
+        } else {
+            this.key = key;
+        }
+
+        if (transmission == null) {
+            this.transmission = "default";
+        } else {
+            this.transmission = transmission;
+        }
+        if (bodyType == null) {
+            this.bodyType = "default";
+        } else {
+            this.bodyType = bodyType;
+        }
+        if (registrationNumber == null) {
+            this.registrationNumber = "х000хх000";
+        } else {
+            this.registrationNumber = registrationNumber;
+        }
+        if (numberOfSeats <= 2) {
+            this.numberOfSeats = 5;
+        } else {
+            this.numberOfSeats = numberOfSeats;
+        }
+        if (rubber == null) {
+            this.rubber = "default";
+        } else {
+            this.rubber = rubber;
+        }
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "defult";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5f;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == null) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+    }
 
     public Auto(String brand, String model, float engineVolume, String color, Integer productionYear, String productionCountry, String transmission, String bodyType, String registrationNumber, Integer numberOfSeats, String rubber) {
+
+
         if (transmission == null) {
             this.transmission = "default";
         } else {
@@ -149,8 +436,12 @@ public class Auto {
         return numberOfSeats;
     }
 
-    public void automobile() {
-        System.out.println(getBrand() + " " + getModel() + ", " + getProductionYear() + " год выпуска, сборка в " + getProductionCountry() + ", " + getColor() + " цвет кузова, объем двигателя - " + getEngineVolume() + ", коробка передач " + getTransmission() + ", тип кузова " + getBodyType() + ", регистрационный номер " + getRegistrationNumber() + ", количество мест " + getNumberOfSeats());
+    public void setKey(Auto.key key) {
+        this.key = key;
+    }
+
+    public void setInsurance(Auto.insurance insurance) {
+        this.insurance = insurance;
     }
 
     public void changeRubber() {
@@ -160,19 +451,36 @@ public class Auto {
             this.rubber = "летняя";
         }
     }
-    public boolean correctRegistrationNumber(){
-        if(registrationNumber.length()!=9){
+
+    public boolean correctRegistrationNumber() {
+        if (registrationNumber.length() != 9) {
             return false;
         }
-        char[] chars=registrationNumber.toCharArray();
-        if(!Character.isAlphabetic(chars[0]) || !Character.isAlphabetic(chars[4]) || !Character.isAlphabetic(chars[5])){
+        char[] chars = registrationNumber.toCharArray();
+        if (!Character.isAlphabetic(chars[0]) || !Character.isAlphabetic(chars[4]) || !Character.isAlphabetic(chars[5])) {
             return false;
         }
-        if(!Character.isDigit(chars[1]) || !Character.isDigit(chars[2]) || !Character.isDigit(chars[3]) || !Character.isDigit(chars[6]) || !Character.isDigit(chars[7]) || !Character.isDigit(chars[8])){
+        if (!Character.isDigit(chars[1]) || !Character.isDigit(chars[2]) || !Character.isDigit(chars[3]) || !Character.isDigit(chars[6]) || !Character.isDigit(chars[7]) || !Character.isDigit(chars[8])) {
             return false;
-        }
-        else {
+        } else {
             return true;
         }
+    }
+
+    public static void information(Auto auto) {
+        System.out.println(auto.getBrand() + " "
+                + auto.getModel() + ", "
+                + auto.getProductionYear() +
+                " год выпуска, сборка в " + auto.getProductionCountry() +
+                ", " + auto.getColor() + " цвет кузова, объем двигателя - " + auto.getEngineVolume() +
+                ", коробка передач " + auto.getTransmission() +
+                ", тип кузова " + auto.getBodyType() +
+                ", регистрационный номер " + auto.getRegistrationNumber() +
+                ", количество мест " + auto.getNumberOfSeats() +
+                " срок действия страховки " + auto.insurance.getValidity()
+                + ", стоимость " + auto.insurance.getPrice() +
+                ", номер страховки " + auto.insurance.getNumber() +
+                ", удаленный запуск двигателя " + auto.key.getRemoteEngineStart()
+                + ", бесключевой доступ " + auto.key.getKeylessEntry());
     }
 }
